a5974cd1affff1ff7650edde46aa390f62f9519e
kelseyjdickson <kelseymedved@gmail.com>
2021-06-16 16:23:32 -0400

    hearts for likes

--- src/components/PinFeed/PinsFeed.js
+++ src/components/PinFeed/PinsFeed.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 //assets
 import './_pinsFeed.scss'
+import heart from '../../assets/heart.png'
 const PinsFeed = ({ pins }) => {
   console.log(pins);
   return (
@@ -10,9 +11,13 @@ const PinsFeed = ({ pins }) => {
       {pins.map(pin => (
         <div key={pin.id}>
             <img className="feed__pin-image"src={pin.images['236x'].url}/>
-            <p>{pin.title}</p>
-            <p>{pin.pinner.full_name}| {pin.pinner.username}</p>
-            
+            <p className="feed__title">{pin.title}</p>
+            <p className="feed__name">{pin.pinner.full_name} | {pin.pinner.username}</p>
+            {/* <img src={pin.pinner.image_large_url}/> */}
+            <div className="feed__likes">
+              <img className="feed__heart" src={heart} alt="likes"/>
+              <p>{pin.like_count}</p>
+              </div>
         </div>
       ))}
     </div>
