314fded1a56d3988908a9aaf030ac6dd34617d0b
kelseyjdickson <kelseymedved@gmail.com>
2021-06-25 11:23:49 -0400

    deleting home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,9 @@
 import React, { useState,useEffect } from 'react'
 import PinsFeed from '../components/PinFeed/PinsFeed'
 const Home = ({ pins }) => {
-    // const [pins, setPins] = useState(null);
-
-    // useEffect(() => {
-    //     fetch("http://localhost:8000/pins")
-    //     .then(res => {
-    //         return res.json();
-    //     })
-    //     .then(data => {
-    //         setPins(data)
-    //     })
-    // }, []);
-   
-
-
     return(
         <>
-            <h1>Home</h1>
+           
             { pins && <PinsFeed pins={pins}/>}
             
         </>
